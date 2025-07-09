048c2e7b811822660bccecbddd667ea8789d170d
Ghislaine-i <g.ineza@gmail.com>
2025-07-09 02:52:18 -0700

    an empty class Square that defines a square.

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+This module defines an empty class named Square.
+"""
+
+class Square:
+    """
+    An empty class that defines a square.
+    """
+    pass # 'pass' is a null operation; it does nothing. It's used here to create an empty class body.
+
