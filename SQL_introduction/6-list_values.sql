3cea5e87c22aa232c3db78a91b0e2dc716f405c2
Ghislaine-i <g.ineza@gmail.com>
2025-07-19 06:14:26 -0700

    a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- This SQL script lists all rows and all fields from the table 'first_table'.
+-- The database name is passed as an argument to the mysql command.
+-- Example usage: mysql -hlocalhost -uroot -p hbtn_0c_0 < 6-list_all_rows.sql
+SELECT *
+FROM first_table;
