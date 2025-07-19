5414b2b6d0301c47a48c4ed1d5e1a9571e90f3c1
Ghislaine-i <g.ineza@gmail.com>
2025-07-19 06:08:50 -0700

    a script that deletes the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- This SQL script lists all tables within a specified database.
+-- The database name is passed as an argument to the mysql command.
+-- For example: mysql -hlocalhost -uroot -p <database_name> < 3-list_tables.sql
+SHOW TABLES;
