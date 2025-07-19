349ad6f71538578bc2830333340ff31a4c982881
Ghislaine-i <g.ineza@alustudent.com>
2025-07-19 20:17:42 +0000

    a  Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script fetches and displays the content of a given URL.
-curl -s "$1"
+# This script calculates the size of a URL's response body.
+body_size_bytes=$(curl -s -o /dev/null -w "%{size_download}" "$1"); echo "Size of response body: ${body_size_bytes} bytes"
