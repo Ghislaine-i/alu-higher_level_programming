80253a6d09b1ad8e8fc4b45e63f8d9f936acd51a
Ghislaine-i <g.ineza@gmail.com>
2025-07-19 06:19:14 -0700

    script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- This SQL script inserts a new row into the 'first_table' in the specified database.
+-- The database name is passed as an argument to the mysql command.
+-- Example usage: mysql -hlocalhost -uroot -p hbtn_0c_0 < 7-insert_new_row.sql
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
