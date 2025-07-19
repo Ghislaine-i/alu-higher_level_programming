33b8ce96394ebd2654bc7d1ee3095d92a8a5e484
Ghislaine-i <g.ineza@gmail.com>
2025-07-19 06:57:43 -0700

    a script that creates the MySQL server user user_0d_1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,16 @@
 -- This SQL script creates the MySQL server user 'user_0d_1'.
 -- The user will have all privileges on the MySQL server.
 -- The password for 'user_0d_1' will be 'user_0d_1_pwd'.
--- If the user already exists, the script will not fail due to the IF NOT EXISTS clause.
+-- If the user 'user_0d_1' already exists, the script will not fail
+-- due to the 'IF NOT EXISTS' clause in the CREATE USER statement.
 
--- Create the user if they don't already exist.
+-- Create the user if they do not already exist.
+-- 'IDENTIFIED BY' sets the password for the user.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to the user.
+-- Grant all privileges on all databases and all tables (*.*)
+-- to the newly created or existing user 'user_0d_1' when connecting from 'localhost'.
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
--- Apply the privilege changes.
+-- Reload the grant tables to ensure that the new privileges take effect immediately.
 FLUSH PRIVILEGES;
