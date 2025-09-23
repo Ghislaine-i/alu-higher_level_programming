57353ea2651699c4456a63025a60f36e3c32c336
Ghislaine-i <g.ineza@gmail.com>
2025-09-23 11:38:00 -0700

    Add Rectangle class with print() method to display rectangle using X

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// 3-rectangle.js
+
+class Rectangle {
+  constructor(w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    // Otherwise, object stays empty
+  }
+
+  print() {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
