d561f63b6087f2bb7f6697fd443ca0378aaa4aa8
Ghislaine-i <g.ineza@gmail.com>
2025-09-23 11:55:42 -0700

    Add function esrever to return reversed version of a list without using reverse()

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,5 @@
-// 8-esrever.js
+#!/usr/bin/node
+// Function that returns the reversed version of a list
 exports.esrever = function (list) {
   const reversed = [];
   for (let i = list.length - 1; i >= 0; i--) {
