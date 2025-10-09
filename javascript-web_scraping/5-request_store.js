704c211fc9bfe6931a37f17cc10d6b09bc713577
Ghislaine-i <g.ineza@gmail.com>
2025-10-09 07:42:38 -0700

    Add script that fetches a webpage and stores content in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,16 +4,16 @@
 const request = require('request');
 const fs = require('fs');
 
-// Get the command-line arguments
+// Get the URL and file path from command-line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Send GET request to the given URL
+// Make a GET request to the given URL
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    // Write the body content to the file in UTF-8 encoding
+    // Write the body to the specified file (UTF-8 encoding)
     fs.writeFile(filePath, body, 'utf8', (err) => {
       if (err) {
         console.log(err);
